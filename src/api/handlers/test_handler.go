d057e4c9a131648ce91fc391ce3a82c77168be30
unknown <reza.refresh1383@gmail.com>
2024-09-04 09:14:50 -0700

    Get different datas

--- src/api/handlers/test_handler.go
+++ src/api/handlers/test_handler.go
@@ -6,6 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type header struct {
+	UserId  string
+	Browser string
+}
+
+type personData struct {
+	FirstName string
+	LastName  string
+}
+
 type TestHandler struct {
 }
 
@@ -15,43 +25,127 @@ func NewTestHandler() *TestHandler {
 
 func (h *TestHandler) Test(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"result" : "Test",
+		"result": "Test",
 	})
 }
 
 func (h *TestHandler) Users(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"result" : "Users",
+		"result": "Users",
 	})
 }
-
 
 func (h *TestHandler) UserById(ctx *gin.Context) {
 	id := ctx.Param("id")
 	ctx.JSON(http.StatusOK, gin.H{
-		"result" : "UserById",
-		"id" : id,
+		"result": "UserById",
+		"id":     id,
 	})
 }
 
 func (h *TestHandler) UserByUsername(ctx *gin.Context) {
 	usrName := ctx.Param("username")
 	ctx.JSON(http.StatusOK, gin.H{
-		"result" : "UserByUsername",
-		"username" : usrName,
+		"result":   "UserByUsername",
+		"username": usrName,
 	})
 }
 
 func (h *TestHandler) Accounts(ctx *gin.Context) {
 	id := ctx.Param("id")
 	ctx.JSON(http.StatusOK, gin.H{
-		"result" : "Accounts",
-		"id" : id,
+		"result": "Accounts",
+		"id":     id,
 	})
 }
 
 func (h *TestHandler) AddUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"result" : "AddUser",
+		"result": "AddUser",
+	})
+}
+
+func (h *TestHandler) HeaderBinder1(ctx *gin.Context) {
+	userId := ctx.GetHeader("UserId")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder1",
+		"userId": userId,
+	})
+}
+
+func (h *TestHandler) HeaderBinder2(ctx *gin.Context) {
+	header := header{}
+	ctx.BindHeader(&header)
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder2",
+		"header": header,
+	})
+}
+
+func (h *TestHandler) QueryBinder1(ctx *gin.Context) {
+	id := ctx.Query("id")
+	name := ctx.Query("name")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder2",
+		"id":     id,
+		"name":   name,
+	})
+}
+
+func (h *TestHandler) QueryBinder2(ctx *gin.Context) {
+	ids := ctx.QueryArray("id")
+	name := ctx.Query("name")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder2",
+		"ids":    ids,
+		"name":   name,
+	})
+}
+
+func (h *TestHandler) UriBinder(ctx *gin.Context) {
+	id := ctx.Param("id")
+	name := ctx.Param("name")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "UriBinder",
+		"id":     id,
+		"name":   name,
+	})
+}
+
+func (h *TestHandler) BodyBinder(ctx *gin.Context) {
+	p := personData{}
+	ctx.ShouldBindJSON(&p)
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "BodyBinder",
+		"person": p,
+	})
+}
+
+func (h *TestHandler) FormBinder(ctx *gin.Context) {
+	p := personData{}
+	ctx.ShouldBind(&p)
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "FormBinder",
+		"person": p,
+	})
+}
+
+func (h *TestHandler) FileBinder(ctx *gin.Context) {
+	file, _ := ctx.FormFile("file")
+	err := ctx.SaveUploadedFile(file, "file")
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "FileBinder",
+		"file":   file.Filename,
 	})
 }
